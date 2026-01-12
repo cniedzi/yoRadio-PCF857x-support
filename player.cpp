a21797e6f91f1f49e92aa51d4d83ef489a80409f
cniedzi <90047136+cniedzi@users.noreply.github.com>
2026-01-12 16:37:59 +0100

    Initial commit

---
+++ player.cpp
@@ -0,0 +1,426 @@
+#include "options.h"
+#include "player.h"
+#include "config.h"
+#include "telnet.h"
+#include "display.h"
+#include "sdmanager.h"
+#include "netserver.h"
+#include "timekeeper.h"
+#include "../displays/tools/l10n.h"
+#include "../pluginsManager/pluginsManager.h"
+#ifdef USE_NEXTION
+#include "../displays/nextion.h"
+#endif
+
+
+Player player;
+QueueHandle_t playerQueue;
+
+/****************  EXTENDER ****************/
+// Mod by C.Niedzinski 2026
+
+#include <Adafruit_PCF8574.h>
+#include <Preferences.h>
+Adafruit_PCF8574 pcf;
+Preferences extenderPreferences;
+
+#define buttonsCount 2 // <<<=== buttons count, max.8 for PCF8574
+#define longPush 1000 // ms
+
+static uint8_t lastButtonPushed = 255;
+unsigned long pushButtonStart; 
+
+
+void handleExtender() {
+    for (uint8_t p = 0; p < buttonsCount; p++) {
+        if (!pcf.digitalRead(p)) {
+          if (lastButtonPushed == 255) {
+            pushButtonStart = millis();
+            lastButtonPushed = p;
+          }
+          break;          
+        }
+        else {
+          if (lastButtonPushed == p) {
+            unsigned long pushButtonTime = millis() - pushButtonStart;
+            uint8_t temp[32] = {0};
+            int offset = p * 2;
+            if (pushButtonTime < longPush) { // SHORT Push - reading station number from flash memory and connection
+              if (extenderPreferences.getBytes("Buttons", temp, 32) == 32) {
+                uint16_t station = (static_cast<uint16_t>(temp[offset + 0])) | (static_cast<uint16_t>(temp[offset + 1]) << 8);
+                Serial.printf("===>>> BUTTON #%d - Retrieved station #%d <<<===\n", p, station);
+                if (station > 0 && station <= config.playlistLength()) {
+                  config.lastStation(station);
+                  player.sendCommand({PR_PLAY, config.lastStation()});
+                }
+              }
+            }
+            else { // LONG Push - saving station to flash memory
+              uint16_t newStation = config.lastStation();
+              Serial.printf("===>>> BUTTON #%d - Saved station #%d <<<===\n", p, newStation);
+              extenderPreferences.getBytes("Buttons", temp, 32);
+              temp[offset] = static_cast<uint8_t>(newStation & 0xFF);
+              temp[offset + 1] = static_cast<uint8_t>((newStation >> 8) & 0xFF);
+              extenderPreferences.putBytes("Buttons", temp, 32);
+            }
+            lastButtonPushed = 255;
+            break;
+          }
+        }
+    }
+}
+/****************  EXTENDER ****************/
+
+
+
+#if VS1053_CS!=255 && !I2S_INTERNAL
+  #if VS_HSPI
+    Player::Player(): Audio(VS1053_CS, VS1053_DCS, VS1053_DREQ, &SPI2) {}
+  #else
+    Player::Player(): Audio(VS1053_CS, VS1053_DCS, VS1053_DREQ, &SPI) {}
+  #endif
+  void ResetChip(){
+    pinMode(VS1053_RST, OUTPUT);
+    digitalWrite(VS1053_RST, LOW);
+    delay(30);
+    digitalWrite(VS1053_RST, HIGH);
+    delay(100);
+  }
+#else
+  #if !I2S_INTERNAL
+    Player::Player() {}
+  #else
+    Player::Player(): Audio(true, I2S_DAC_CHANNEL_BOTH_EN)  {}
+  #endif
+#endif
+
+
+void Player::init() {
+  Serial.print("##[BOOT]#\tplayer.init\t");
+  playerQueue=NULL;
+  _resumeFilePos = 0;
+  _hasError=false;
+  playerQueue = xQueueCreate( 5, sizeof( playerRequestParams_t ) );
+  setOutputPins(false);
+  delay(50);
+#ifdef MQTT_ROOT_TOPIC
+  memset(burl, 0, MQTT_BURL_SIZE);
+#endif
+  if(MUTE_PIN!=255) pinMode(MUTE_PIN, OUTPUT);
+  #if I2S_DOUT!=255
+    #if !I2S_INTERNAL
+      setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    #endif
+  #else
+    SPI.begin();
+    if(VS1053_RST>0) ResetChip();
+    begin();
+  #endif
+  setBalance(config.store.balance);
+  setTone(config.store.bass, config.store.middle, config.store.trebble);
+  setVolume(0);
+  _status = STOPPED;
+  _volTimer=false;
+  //randomSeed(analogRead(0));
+  #if PLAYER_FORCE_MONO
+    forceMono(true);
+  #endif
+  _loadVol(config.store.volume);
+  setConnectionTimeout(CONNECTION_TIMEOUT, CONNECTION_TIMEOUT_SSL);
+  Serial.println("DONE");
+
+
+
+  
+  /****************  EXTENDER ****************/
+  if (!extenderPreferences.begin("extender", false)) {
+    Serial.println("Flash memory error");
+    while (1);
+  }
+  
+  if (!pcf.begin(0x20, &Wire)) {
+    Serial.println("PCF8574 not found :(");
+    while (1);
+  }
+
+  for (uint8_t p = 0; p < buttonsCount; p++) {
+    pcf.pinMode(p, INPUT);
+  }
+  
+  Serial.println("PCF8574 found :)");
+  /****************  EXTENDER ****************/
+
+
+}
+
+void Player::sendCommand(playerRequestParams_t request){
+  if(playerQueue==NULL) return;
+  xQueueSend(playerQueue, &request, PLQ_SEND_DELAY);
+}
+
+void Player::resetQueue(){
+  if(playerQueue!=NULL) xQueueReset(playerQueue);
+}
+
+void Player::stopInfo() {
+  config.setSmartStart(0);
+  netserver.requestOnChange(MODE, 0);
+}
+
+void Player::setError(){
+  _hasError=true;
+  config.setTitle(config.tmpBuf);
+  telnet.printf("##ERROR#:\t%s\n", config.tmpBuf);
+}
+
+void Player::setError(const char *e){
+  strlcpy(config.tmpBuf, e, sizeof(config.tmpBuf));
+  setError();
+}
+
+void Player::_stop(bool alreadyStopped){
+  log_i("%s called", __func__);
+  if(config.getMode()==PM_SDCARD && !alreadyStopped) config.sdResumePos = player.getFilePos();
+  _status = STOPPED;
+  setOutputPins(false);
+  if(!_hasError) config.setTitle((display.mode()==LOST || display.mode()==UPDATING)?"":LANG::const_PlStopped);
+  config.station.bitrate = 0;
+  config.setBitrateFormat(BF_UNKNOWN);
+  #ifdef USE_NEXTION
+    nextion.bitrate(config.station.bitrate);
+  #endif
+  setDefaults();
+  if(!alreadyStopped) stopSong();
+  netserver.requestOnChange(BITRATE, 0);
+  display.putRequest(DBITRATE);
+  display.putRequest(PSTOP);
+  //setDefaults();
+  //if(!alreadyStopped) stopSong();
+  if(!lockOutput) stopInfo();
+  if (player_on_stop_play) player_on_stop_play();
+  pm.on_stop_play();
+}
+
+void Player::initHeaders(const char *file) {
+  if(strlen(file)==0 || true) return; //TODO Read TAGs
+  connecttoFS(sdman,file);
+  eofHeader = false;
+  while(!eofHeader) Audio::loop();
+  //netserver.requestOnChange(SDPOS, 0);
+  setDefaults();
+}
+void resetPlayer(){
+  if(!config.store.watchdog) return;
+  player.resetQueue();
+  player.sendCommand({PR_STOP, 0});
+  player.loop();
+}
+
+#ifndef PL_QUEUE_TICKS
+  #define PL_QUEUE_TICKS 0
+#endif
+#ifndef PL_QUEUE_TICKS_ST
+  #define PL_QUEUE_TICKS_ST 15
+#endif
+void Player::loop() {
+  if(playerQueue==NULL) return;
+  playerRequestParams_t requestP;
+  if(xQueueReceive(playerQueue, &requestP, isRunning()?PL_QUEUE_TICKS:PL_QUEUE_TICKS_ST)){
+    switch (requestP.type){
+      case PR_STOP: _stop(); break;
+      case PR_PLAY: {
+        if (requestP.payload>0) {
+          config.setLastStation((uint16_t)requestP.payload);
+        }
+        _play((uint16_t)abs(requestP.payload)); 
+        if (player_on_station_change) player_on_station_change(); 
+        pm.on_station_change();
+        break;
+      }
+      case PR_TOGGLE: {
+        toggle();
+        break;
+      }
+      case PR_VOL: {
+        config.setVolume(requestP.payload);
+        Audio::setVolume(volToI2S(requestP.payload));
+        break;
+      }
+      #ifdef USE_SD
+      case PR_CHECKSD: {
+        if(config.getMode()==PM_SDCARD){
+          if(!sdman.cardPresent()){
+            sdman.stop();
+            config.changeMode(PM_WEB);
+          }
+        }
+        break;
+      }
+      #endif
+      case PR_VUTONUS: {
+        if(config.vuThreshold>10) config.vuThreshold -=10;
+        break;
+      }
+      case PR_BURL: {
+      #ifdef MQTT_ROOT_TOPIC
+        if(strlen(burl)>0){
+          browseUrl();
+        }
+      #endif
+        break;
+      }
+          
+      default: break;
+    }
+  }
+  Audio::loop();
+  if(!isRunning() && _status==PLAYING) _stop(true);
+  if(_volTimer){
+    if((millis()-_volTicks)>3000){
+      config.saveVolume();
+      _volTimer=false;
+    }
+  }
+  /*
+#ifdef MQTT_ROOT_TOPIC
+  if(strlen(burl)>0){
+    browseUrl();
+  }
+#endif*/
+
+
+/****************  EXTENDER ****************/
+handleExtender();
+/****************  EXTENDER ****************/
+
+
+}
+
+void Player::setOutputPins(bool isPlaying) {
+  if(REAL_LEDBUILTIN!=255) digitalWrite(REAL_LEDBUILTIN, LED_INVERT?!isPlaying:isPlaying);
+  bool _ml = MUTE_LOCK?!MUTE_VAL:(isPlaying?!MUTE_VAL:MUTE_VAL);
+  if(MUTE_PIN!=255) digitalWrite(MUTE_PIN, _ml);
+}
+
+void Player::_play(uint16_t stationId) {
+  log_i("%s called, stationId=%d", __func__, stationId);
+  _hasError=false;
+  setDefaults();
+  _status = STOPPED;
+  setOutputPins(false);
+  remoteStationName = false;
+  
+  if(!config.prepareForPlaying(stationId)) return;
+  _loadVol(config.store.volume);
+  
+  bool isConnected = false;
+  if(config.getMode()==PM_SDCARD && SDC_CS!=255){
+    isConnected=connecttoFS(sdman,config.station.url,config.sdResumePos==0?_resumeFilePos:config.sdResumePos-player.sd_min);
+  }else {
+    config.saveValue(&config.store.play_mode, static_cast<uint8_t>(PM_WEB));
+  }
+  connproc = false;
+  if(config.getMode()==PM_WEB) isConnected=connecttohost(config.station.url);
+  connproc = true;
+  if(isConnected){
+    _status = PLAYING;
+    config.configPostPlaying(stationId);
+    setOutputPins(true);
+    if (player_on_start_play) player_on_start_play();
+    pm.on_start_play();
+  }else{
+    telnet.printf("##ERROR#:\tError connecting to %.128s\n", config.station.url);
+    snprintf(config.tmpBuf, sizeof(config.tmpBuf), "Error connecting to %.128s", config.station.url); setError();
+    _stop(true);
+  };
+}
+
+#ifdef MQTT_ROOT_TOPIC
+void Player::browseUrl(){
+  _hasError=false;
+  remoteStationName = true;
+  config.setDspOn(1);
+  resumeAfterUrl = _status==PLAYING;
+  display.putRequest(PSTOP);
+  setOutputPins(false);
+  config.setTitle(LANG::const_PlConnect);
+  if (connecttohost(burl)){
+    _status = PLAYING;
+    config.setTitle("");
+    netserver.requestOnChange(MODE, 0);
+    setOutputPins(true);
+    display.putRequest(PSTART);
+    if (player_on_start_play) player_on_start_play();
+    pm.on_start_play();
+  }else{
+    telnet.printf("##ERROR#:\tError connecting to %.128s\n", burl);
+    snprintf(config.tmpBuf, sizeof(config.tmpBuf), "Error connecting to %.128s", burl); setError();
+    _stop(true);
+  }
+  //memset(burl, 0, MQTT_BURL_SIZE);
+}
+#endif
+
+void Player::prev() {
+  uint16_t lastStation = config.lastStation();
+  if(config.getMode()==PM_WEB || !config.store.sdsnuffle){
+    if (lastStation == 1) config.lastStation(config.playlistLength()); else config.lastStation(lastStation-1);
+  }
+  sendCommand({PR_PLAY, config.lastStation()});
+}
+
+void Player::next() {
+  uint16_t lastStation = config.lastStation();
+  if(config.getMode()==PM_WEB || !config.store.sdsnuffle){
+    if (lastStation == config.playlistLength()) config.lastStation(1); else config.lastStation(lastStation+1);
+  }else{
+    config.lastStation(random(1, config.playlistLength()));
+  }
+  sendCommand({PR_PLAY, config.lastStation()});
+}
+
+void Player::toggle() {
+  if (_status == PLAYING) {
+    sendCommand({PR_STOP, 0});
+  } else {
+    sendCommand({PR_PLAY, config.lastStation()});
+  }
+}
+
+void Player::stepVol(bool up) {
+  if (up) {
+    if (config.store.volume <= 254 - config.store.volsteps) {
+      setVol(config.store.volume + config.store.volsteps);
+    }else{
+      setVol(254);
+    }
+  } else {
+    if (config.store.volume >= config.store.volsteps) {
+      setVol(config.store.volume - config.store.volsteps);
+    }else{
+      setVol(0);
+    }
+  }
+}
+
+uint8_t Player::volToI2S(uint8_t volume) {
+  int vol = map(volume, 0, 254 - config.station.ovol * 3 , 0, 254);
+  if (vol > 254) vol = 254;
+  if (vol < 0) vol = 0;
+  return vol;
+}
+
+void Player::_loadVol(uint8_t volume) {
+  setVolume(volToI2S(volume));
+}
+
+void Player::setVol(uint8_t volume) {
+  _volTicks = millis();
+  _volTimer = true;
+  player.sendCommand({PR_VOL, volume});
+}
+
+
+
+
+
