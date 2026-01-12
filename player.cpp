87945e746af2400bf2c4bf0242607d347eb6b28a
cniedzi <90047136+cniedzi@users.noreply.github.com>
2026-01-12 20:53:39 +0100

    v.1.01 - added comment

--- player.cpp
+++ player.cpp
@@ -26,7 +26,7 @@ Adafruit_PCF8574 pcf;
 Preferences extenderPreferences;
 
 #define EXPANDER_PORTS 8 // number of ports of PCF8574
-#define buttonsCount 2 // <<<=== buttons count, max.8 for PCF8574
+#define buttonsCount 2 // <<<=== buttons count, max.8 for PCF8574; IMPORTANT: all buttons must be externally pulled-up!
 #define longPush 1000 // ms
 
 static uint8_t lastButtonPushed = 255;
